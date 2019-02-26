277181623da6413d6832188d0b52b06fd4c82d13
iamcxnoguigan <747113809@qq.com>
2019-02-26 17:34:50 +0800

    rm

---
+++ shipping-company/workflow/vessel-enterprise-rb-service/vessel-enterprise-rb-service/src/main/java/com/l2l/enterprise/vessel/extension/activiti/annotation/AnnotationUtils.java
@@ -0,0 +1,100 @@
+package com.l2l.enterprise.vessel.extension.activiti.annotation;
+
+import com.l2l.enterprise.vessel.extension.activiti.model.Annotation;
+import com.l2l.enterprise.vessel.extension.activiti.parser.AnnotationConstants;
+import org.activiti.bpmn.model.*;
+import org.activiti.bpmn.model.Process;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class AnnotationUtils {
+    private static final String SET_PREFIX = "set";
+    private static final String IS_PREFIX = "is";
+    private static final String GET_PREFIX = "get";
+    private static final Logger log = LoggerFactory.getLogger(AnnotationUtils.class);
+
+    public static List<Annotation> collectAnnotationsOnElement(FlowElement flowElement){
+
+//        Map<String , List<ExtensionElement>> extensionElements = flowElement.getExtensionElements();
+
+        List<Annotation> annotations = new ArrayList<Annotation>();
+        List<ExtensionElement> extensionElements = flowElement.getExtensionElements().entrySet().stream()
+            .filter(e -> e.getKey().equals(AnnotationConstants.ELEMENT_NAME))
+            .collect(
+                () -> new ArrayList<ExtensionElement>() ,
+                (r , e) ->{ r.addAll(e.getValue()); },
+                (l, r)-> {l.addAll(r);});
+
+        if(extensionElements != null){
+            for (ExtensionElement extensionElement : extensionElements){
+                Collection<List<ExtensionAttribute>> attrsCollection = extensionElement.getAttributes().values();
+                Iterator<List<ExtensionAttribute>> it = attrsCollection.iterator();
+                Annotation annotaion = new Annotation();
+                annotaion.setTargetElementId(flowElement.getId());
+                while (it.hasNext()){
+                    ExtensionAttribute tAttr= it.next().get(0);//no duplicate attributes by default
+                    if(tAttr != null){
+                        reflectFillAnnotationFiled(tAttr.getName().trim(), tAttr.getValue().trim(), annotaion ,Annotation.class);
+                    }
+                }
+                annotations.add(annotaion);
+            }
+        }
+         return annotations;
+       }
+
+       public static List<MsgAnnotation> collectMsgAnnoationOnProcess(Process process){
+           List<MsgAnnotation> msgAnnotations = new ArrayList<MsgAnnotation>();
+           List<ExtensionElement> extensionElements = process.getExtensionElements().entrySet().stream()
+               .filter(e -> e.getKey().equals(AnnotationConstants.ELEMENT_NAME))
+               .collect(
+                   () -> new ArrayList<ExtensionElement>() ,
+                   (r , e) ->{ r.addAll(e.getValue()); },
+                   (l, r)-> {l.addAll(r);});
+           if(extensionElements != null){
+               for (ExtensionElement extensionElement : extensionElements){
+                   Collection<List<ExtensionAttribute>> attrsCollection = extensionElement.getAttributes().values();
+                   Iterator<List<ExtensionAttribute>> it = attrsCollection.iterator();
+                   MsgAnnotation msgAnnotation = new MsgAnnotation();
+                   msgAnnotation.setTargetElementId(process.getId());
+                   while (it.hasNext()){
+                       ExtensionAttribute tAttr= it.next().get(0);//no duplicate attributes by default
+                       if(tAttr != null){
+                           reflectFillAnnotationFiled(tAttr.getName().trim(), tAttr.getValue().trim(), msgAnnotation , MsgAnnotation.class);
+                       }
+                   }
+                   msgAnnotations.add(msgAnnotation);
+               }
+           }
+            return msgAnnotations;
+       }
+
+       public static void reflectFillAnnotationFiled(String attrName , Object attrVal , Annotation annotation ,Class<?> clazz){
+           Method setMethod = null;
+           Method getMethod = null;
+           String methodSuffix=methodSuffix = attrName.substring(0,1).toUpperCase()+attrName.substring(1);
+           try {
+               Field field = clazz.getField(attrName);
+               setMethod = clazz.getMethod(SET_PREFIX+methodSuffix , new Class[]{field.getType()});
+               setMethod.invoke(annotation , new Object[]{attrVal});
+           } catch (NoSuchFieldException e) {
+               e.printStackTrace();
+           } catch (NoSuchMethodException e) {
+               e.printStackTrace();
+           } catch (IllegalAccessException e) {
+               e.printStackTrace();
+           } catch (InvocationTargetException e) {
+               e.printStackTrace();
+           }
+       }
+
+
+
+}
---
+++ shipping-company/workflow/vessel-enterprise-rb-service/vessel-enterprise-rb-service/src/main/java/com/l2l/enterprise/vessel/web/rest/TestController.java
@@ -0,0 +1,159 @@
+package com.l2l.enterprise.vessel.web.rest;
+
+import com.l2l.enterprise.vessel.extension.activiti.form.FormDefinition;
+import com.l2l.enterprise.vessel.extension.activiti.form.FormService;
+import com.l2l.enterprise.vessel.service.L2LTaskRuntimeImpl;
+import org.activiti.bpmn.model.ActivitiListener;
+import org.activiti.bpmn.model.FlowElement;
+import org.activiti.bpmn.model.ServiceTask;
+import org.activiti.cloud.alfresco.data.domain.AlfrescoPagedResourcesAssembler;
+import org.activiti.cloud.services.core.ActivitiForbiddenException;
+import org.activiti.cloud.services.core.ProcessDiagramGeneratorWrapper;
+import org.activiti.cloud.services.core.pageable.SpringPageConverter;
+import org.activiti.cloud.services.rest.api.ProcessDefinitionController;
+import org.activiti.cloud.services.rest.api.resources.ProcessDefinitionResource;
+import org.activiti.cloud.services.rest.api.resources.ProcessInstanceResource;
+import org.activiti.cloud.services.rest.api.resources.TaskResource;
+import org.activiti.cloud.services.rest.assemblers.ProcessInstanceResourceAssembler;
+import org.activiti.cloud.services.rest.assemblers.TaskResourceAssembler;
+import org.activiti.engine.ActivitiObjectNotFoundException;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.impl.context.Context;
+import org.activiti.engine.task.TaskQuery;
+import org.activiti.image.exception.ActivitiInterchangeInfoNotFoundException;
+import org.activiti.runtime.api.NotFoundException;
+import org.activiti.runtime.api.ProcessRuntime;
+import org.activiti.runtime.api.TaskRuntime;
+import org.activiti.runtime.api.model.ProcessDefinition;
+import org.activiti.runtime.api.model.ProcessInstance;
+import org.activiti.runtime.api.model.Task;
+import org.activiti.runtime.api.model.builders.TaskPayloadBuilder;
+import org.activiti.runtime.api.model.impl.BPMNActivityImpl;
+import org.activiti.runtime.api.model.payloads.CompleteTaskPayload;
+import org.activiti.runtime.api.model.payloads.StartProcessPayload;
+import org.activiti.runtime.api.query.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.*;
+
+@RestController
+public class TestController {
+    private final RepositoryService repositoryService;
+    private final ProcessDiagramGeneratorWrapper processDiagramGenerator;
+    private final ProcessInstanceResourceAssembler resourceAssembler;
+    private final AlfrescoPagedResourcesAssembler<ProcessInstance> pagedResourcesAssembler;
+    private final ProcessRuntime processRuntime;
+    private final SpringPageConverter pageConverter;
+//    private final RepositoryService;
+    @Autowired
+    private FormService formService;
+    @ExceptionHandler({ActivitiForbiddenException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleAppException(ActivitiForbiddenException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler({ActivitiObjectNotFoundException.class, NotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleAppException(RuntimeException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler({ActivitiInterchangeInfoNotFoundException.class})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String handleActivitiInterchangeInfoNotFoundException(ActivitiInterchangeInfoNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @Autowired
+    public TestController(RepositoryService repositoryService, ProcessDiagramGeneratorWrapper processDiagramGenerator, ProcessInstanceResourceAssembler resourceAssembler, AlfrescoPagedResourcesAssembler<ProcessInstance> pagedResourcesAssembler, ProcessRuntime processRuntime, SpringPageConverter pageConverter) {
+        this.repositoryService = repositoryService;
+        this.processDiagramGenerator = processDiagramGenerator;
+        this.resourceAssembler = resourceAssembler;
+        this.pagedResourcesAssembler = pagedResourcesAssembler;
+        this.processRuntime = processRuntime;
+        this.pageConverter = pageConverter;
+    }
+    @RequestMapping(
+        value = {"/v2/process-instances/{processDefinitionId}"},
+        method = {RequestMethod.POST}
+    )
+    public ProcessInstanceResource testHello(@PathVariable("processDefinitionId") String processDefinitionId){
+        String processDefinitionKey = "myProcess_1";
+        String processInstanceName = "myProcess_1";
+        StartProcessPayload startProcessPayload = new StartProcessPayload(processDefinitionId , processDefinitionKey , processInstanceName , null , null);
+        return this.resourceAssembler.toResource(this.processRuntime.start(startProcessPayload));
+    }
+
+    @RequestMapping(
+        value = {"/v2/process-instances/withvar/{processDefinitionId}"},
+        method = {RequestMethod.POST}
+    )
+    public ProcessInstanceResource startProcessinstanceWithVariables(@PathVariable("processDefinitionId") String processDefinitionId ,@RequestBody Map<String,Object> varMap){
+        String processDefinitionKey = "myProcess_1";
+        String processInstanceName = "myProcess_1";
+        StartProcessPayload startProcessPayload = new StartProcessPayload(processDefinitionId , processDefinitionKey , processInstanceName , null , varMap);
+        return this.resourceAssembler.toResource(this.processRuntime.start(startProcessPayload));
+    }
+
+
+    @Autowired
+    L2LTaskRuntimeImpl l2LTaskRuntime;
+    @Autowired
+    TaskResourceAssembler taskResourceAssembler;
+    @RequestMapping(
+        value = {"v2/{taskId}/complete"},
+        method = {RequestMethod.POST}
+    )
+    public TaskResource completeTask(@PathVariable String taskId, @RequestBody(required = false) CompleteTaskPayload completeTaskPayload) {
+        if (completeTaskPayload == null) {
+            completeTaskPayload = TaskPayloadBuilder.complete().withTaskId(taskId).build();
+        } else {
+            completeTaskPayload.setTaskId(taskId);
+        }
+
+        Task task = this.l2LTaskRuntime.complete(completeTaskPayload);
+        return this.taskResourceAssembler.toResource(task);
+    }
+
+    @RequestMapping(
+        value = {"/v2/startform/{processDefinitionId}"},
+        method = {RequestMethod.GET}
+    )
+    public FormDefinition getStartFormDefinition(@PathVariable("processDefinitionId") String processDefinitionId){
+        return this.formService.getStartForm(processDefinitionId);
+    }
+    @Autowired
+    TaskService taskService;
+    @RequestMapping("/v2/gettaskform/{taskId}")
+    public FormDefinition gettaskForm(@PathVariable("taskId") String taskId){
+        org.activiti.engine.task.Task internalTask = (org.activiti.engine.task.Task)((TaskQuery)this.taskService.createTaskQuery().taskId(taskId)).singleResult();
+        String taskDefinitionKey = internalTask.getTaskDefinitionKey();
+        String processDefinitionId = internalTask.getProcessDefinitionId();
+        return this.formService.getUserTaskForm(processDefinitionId,taskDefinitionKey);
+    }
+    @Autowired
+    SenderTest senderTest;
+    @RequestMapping("/sender")
+    public void sender(){
+        senderTest.sinkSenderTester();
+    }
+    @Autowired
+    BpmnAdd bpmnAdd;
+    @RequestMapping(
+        value = {"/v2/bpmn"},
+        method = {RequestMethod.POST}
+    )
+    public String bpmnFileChange(@RequestBody Map<String,String> fileMap) throws IOException {
+        bpmnAdd.changBpmn(fileMap.get("oldfile"),fileMap.get("newfile"));
+        return "ok";
+    }
+
+}
